334bac0d13c6946ea5dc1fd341721815036f5fdf
amirhussain8965 <amirhussain8965@gmail.com>
2026-04-08 22:33:02 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header_section");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopBtn = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTopBtn.classList.add("show");
+        } else {
+            backToTopBtn.classList.remove("show");
+        }
+    });
+
+    backToTopBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
